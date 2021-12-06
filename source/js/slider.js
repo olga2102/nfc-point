17721cca8c2196a36ef28c9f12cf5802093947a7
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-12-06 23:47:52 +0300

    youtube

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,16 +1,37 @@
-new Swiper(".swiper", {
+new Swiper('.shop-swiper', {
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.shop-button--next',
+    prevEl: '.shop-button--prev',
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: '.shop-pagination',
     clickable: true,
   },
   breakpoints: {
     360: {
     slidesPerView: 1,
     },
+  },
+  spaceBetween: 20,
+  loop: true,
+});
+
+new Swiper('.youtube-swiper', {
+  // navigation: {
+  //   nextEl: '.shop-button--next',
+  //   prevEl: '.shop-button--prev',
+  // },
+  // pagination: {
+  //   el: '.shop-pagination',
+  //   clickable: true,
+  // },
+  breakpoints: {
+    360: {
+    slidesPerView: 2,
+    },
+    940: {
+      slidesPerView: 3,
+    }
   },
   spaceBetween: 20,
   loop: true,
