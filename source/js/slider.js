af71f4efaf47d57636c7cd60b28aa4a29ac0210c
olga2102 <70343745+olga2102@users.noreply.github.com>
2021-12-07 22:25:36 +0300

    us partners feedback articles

--- source/js/slider.js
+++ source/js/slider.js
@@ -36,3 +36,45 @@ new Swiper('.youtube-swiper', {
   spaceBetween: 20,
   loop: true,
 });
+
+new Swiper('.articles-swiper', {
+  // navigation: {
+  //   nextEl: '.shop-button--next',
+  //   prevEl: '.shop-button--prev',
+  // },
+  // pagination: {
+  //   el: '.shop-pagination',
+  //   clickable: true,
+  // },
+  breakpoints: {
+    360: {
+    slidesPerView: 2,
+    },
+    940: {
+      slidesPerView: 3,
+    }
+  },
+  spaceBetween: 20,
+  loop: true,
+});
+
+new Swiper('.partners-swiper', {
+  // navigation: {
+  //   nextEl: '.shop-button--next',
+  //   prevEl: '.shop-button--prev',
+  // },
+  // pagination: {
+  //   el: '.shop-pagination',
+  //   clickable: true,
+  // },
+  breakpoints: {
+    360: {
+    slidesPerView: 4,
+    },
+    940: {
+      slidesPerView: 6,
+    }
+  },
+  spaceBetween: 20,
+  loop: true,
+});
